c484a6085537fe773769472c1346ca12e82a4926
tanmoyjana <tanmoyjana@grmtech.com>
2021-10-05 20:30:10 +0530

    removed unued css

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 //import '../node_modules/bootstrap/dist/css/bootstrap-theme.css'
 import '../public/css/font-awesome-custom-4.7.css'
-import 'next-pagination/dist/index.css'
+//import 'next-pagination/dist/index.css'
 
 import '../styles/globals.css'
-import '../styles/practices.css'
-import '../public/css/education.css'
+//import '../styles/practices.css'
+//import '../public/css/education.css'
 import '../public/css/misc.css'
-import '../styles/reviews.css'
-import '../public/css/trusts.css'
+//import '../styles/reviews.css'
+//import '../public/css/trusts.css'
 
 
 function MyApp({ Component, pageProps }) {
